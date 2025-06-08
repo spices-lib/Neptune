275ae09b18ceb32b28e15518126e2d0517182401
Spiecs <spiecs@outlook.com>
2025-06-08 14:59:20 +0800

    preview finish

--- Nepnep/vite.config.js
+++ Nepnep/vite.config.js
@@ -1,9 +1,10 @@
+import { defineConfig } from 'vite'
 import mkcert from "vite-plugin-mkcert";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 const isCodeSandbox = !!process.env.SANDBOX_URL
 
-export default {
+export default defineConfig({
     root : 'src/',
     publicDir: '../static/',
     base: './',
@@ -17,10 +18,20 @@ export default {
     {
         outDir: '../dist',
         emptyOutDir: true,
-        sourcemap: true
+        sourcemap: true,
+        target: 'es2022'
+    },
+    preview: {
+        https: true,
+        // vite build do not use the plugin: crossOriginIsolation, add headers here.
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Resource-Policy': 'same-origin'
+        }
     },
     plugins: [
         mkcert(),               // Enable https
         crossOriginIsolation()  // Enable shared buffer
     ]
-}
+})
