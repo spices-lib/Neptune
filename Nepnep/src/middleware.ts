0f6e7b1e15e3db4d8162a491e3e5ebbdea00054c
Spiecs <spiecs@outlook.com>
2025-09-13 21:40:39 +0800

    reduce middleware size

--- Nepnep/src/middleware.ts
+++ Nepnep/src/middleware.ts
@@ -1,13 +1,18 @@
-import { auth } from './server/auth'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export default auth((req) => {
-    const isAuthenticated = !!req.auth
+export function middleware(request: NextRequest) {
 
-    if(!isAuthenticated) {
-        const newUrl = new URL('./signin', req.nextUrl.origin)
-        return Response.redirect(newUrl)
+    const hasAuthSession = request.cookies.has('authjs.session-token')
+    const isSignInPage = request.nextUrl.pathname === '/signin'
+    
+    if (!isSignInPage && !hasAuthSession) {
+        const url = new URL('/signin', request.url)
+        return NextResponse.redirect(url)
     }
-})
+
+    return NextResponse.next()
+}
 
 export const config = {
     matcher: ['/dashboard', '/dashboard/:path*']
