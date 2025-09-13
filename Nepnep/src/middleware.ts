5d65c79af07da6b8cd4a8ebd35eb0de5f5f74e17
Spiecs <spiecs@outlook.com>
2025-09-13 21:54:53 +0800

    Update middleware.ts

--- Nepnep/src/middleware.ts
+++ Nepnep/src/middleware.ts
@@ -3,7 +3,10 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
 
-    const hasAuthSession = request.cookies.has('authjs.session-token')
+    const hasAuthSession =
+        request.cookies.has('authjs.session-token') ||
+        request.cookies.has('__Secure-authjs.session-token')
+    
     const isSignInPage = request.nextUrl.pathname === '/signin'
     
     if (!isSignInPage && !hasAuthSession) {
