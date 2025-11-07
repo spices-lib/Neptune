cfba0d31269e7b5ebe07f218bd36806b21067bc0
Spiecs <spiecs@outlook.com>
2025-11-07 21:15:28 +0800

    script fix

--- Scripts/BuildEntry/Wasm/build_release.bat
+++ Scripts/BuildEntry/Wasm/build_release.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM To build system directory
-cd "../BuildSystem"
+cd "../../BuildSystem"
 
 REM Build with configure
 call python SolutionBuilder.py --build_system=premake --platform=emscripten --toolset=GNU --behave=build-release
--- Scripts/BuildEntry/Windows/generate_vs.bat
+++ Scripts/BuildEntry/Windows/generate_vs.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM To build system directory
-cd "../BuildSystem"
+cd "../../BuildSystem"
 
 REM Build with configure
 call python SolutionBuilder.py --build_system=premake --platform=windows --toolset=vs --behave=generate
