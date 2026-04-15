a4b8308bbb09375481a96b837ff91009dad56cd5
Spices <spiecs@outlook.com>
2026-04-16 00:09:12 +0800

    feat: Add d3d11/d3d12/metal render backend

---
+++ Neptune/src/Render/Backend/Direct3D11/RenderBackend.cpp
@@ -0,0 +1,58 @@
+/**
+* @file RenderBackend.cpp.
+* @brief The RenderBackend Class Implementation.
+* @author Spices.
+*/
+
+#include "Pchheader.h"
+
+#ifndef NP_PLATFORM_EMSCRIPTEN
+
+#include "RenderBackend.h"
+
+namespace Neptune::Direct3D11 {
+
+    RenderBackend::RenderBackend()
+        : RenderFrontend(RenderBackendEnum::Direct3D11)
+    {
+        NEPTUNE_PROFILE_ZONE
+    }
+
+    void RenderBackend::OnInitialize()
+    {
+        NEPTUNE_PROFILE_ZONE
+
+        RenderFrontend::OnInitialize();
+    }
+
+    void RenderBackend::OnShutDown()
+    {
+        NEPTUNE_PROFILE_ZONE
+
+        RenderFrontend::OnShutDown();
+    }
+
+    void RenderBackend::BeginFrame(Scene* scene)
+    {
+        NEPTUNE_PROFILE_ZONE
+    }
+
+    void RenderBackend::EndFrame(Scene* scene)
+    {
+        NEPTUNE_PROFILE_ZONE
+    }
+
+    void RenderBackend::Wait()
+    {
+        NEPTUNE_PROFILE_ZONE
+    }
+
+    std::any RenderBackend::CreateRHI(RHI::ERHI e, void* payload)
+	{
+        NEPTUNE_PROFILE_ZONE
+
+        return nullptr;
+	}
+}
+
+#endif
---
+++ Neptune/src/Render/Backend/Direct3D11/RenderBackend.h
@@ -0,0 +1,83 @@
+/**
+* @file RenderBackend.h.
+* @brief The RenderBackend Class Definitions.
+* @author Spices.
+*/
+
+#pragma once
+
+#ifndef NP_PLATFORM_EMSCRIPTEN
+
+#include "Core/Core.h"
+#include "Render/Frontend/RenderFrontend.h"
+
+namespace Neptune {
+
+    class Scene;
+}
+
+namespace Neptune::Direct3D11 {
+
+    /**
+    * @brief RenderBackend Class.
+    * This class defines the RenderBackend behaves.
+    */
+    class RenderBackend : public RenderFrontend
+    {
+    public:
+
+        /**
+        * @brief Constructor Function.
+        */
+        RenderBackend();
+
+        /**
+        * @brief Destructor Function.
+        */
+        ~RenderBackend() override = default;
+
+        /**
+        * @brief Interface of Initialize.
+        */
+        void OnInitialize() override;
+
+        /**
+        * @brief Interface of ShutDown.
+        */
+        void OnShutDown() override;
+
+        /**
+        * @brief Interface of Begin a frame.
+        *
+        * @param[in] scene Scene.
+        */
+        void BeginFrame(Scene* scene) override;
+
+        /**
+        * @brief Interface of End a frame.
+        *
+        * @param[in] scene Scene.
+        */
+        void EndFrame(Scene* scene) override;
+
+        /**
+        * @brief Interface of Wait RenderBackend idle.
+        */
+        void Wait() override;
+
+        /**
+        * @brief Interface of CreateRHI.
+        *
+        * @param[in] e ERHI.
+        * @param[in] payload RHI Payload.
+        *
+        * @return Returns RHI::Impl
+        */
+        std::any CreateRHI(RHI::ERHI e, void* payload) override;
+
+    private:
+
+    };
+}
+
+#endif
